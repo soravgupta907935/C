eb11a69e0f24fd1fb2b0815b862ca6686efc411d
Dhruvin Dhupia <dhupiadhruvin4@gmail.com>
2025-11-23 00:36:39 +0530

    first commit

---
+++ first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int a,b,multiply;
+   printf(" write a value ");
+   scanf("%d",&a);
+
+   printf(" write b value ");
+   scanf("%d",&b);
+
+   multiply=a*b;
+   printf("multiply=%d",multiply);
+
+   return 0;
+}
